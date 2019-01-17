2576ef9ce47970ba8c41a81b33a888274636256f
gordonzhang <gordonzhang81@gmail.com>
2019-01-17 15:12:57 -0500

    Update AggregatingNeighbors.py
    completed calculation of happiness matrix

--- AggregatingNeighbors.py
+++ AggregatingNeighbors.py
@@ -35,15 +35,6 @@ def initializeMatrix(nWidth, occupRate, groupRatios):
 	return np.reshape(nodes, (nWidth, nWidth))
 
 
-nWidth = 10
-occupRate = 0.9
-groupRatios = [.5, .5]
-likeMat = []
-
-initMap = initializeMatrix(nWidth, occupRate, groupRatios)
-print(initMap)
-
-
 def calcHappiness(initMap, weightMat, likeMat):
 	assert (np.size(weightMat,0)%2 == 1 and np.size(weightMat,1)%2 == 1),...
 	"Number of rows and columns of weight matrix should be both odd numbers."
@@ -56,37 +47,79 @@ def calcHappiness(initMap, weightMat, likeMat):
 	rVer = int(round(rVer))
 	rHor = int(round(rHor))
 	
-	happMt = np.zeros_like(initMap)
+	happMat = np.zeros_like(initMap, float)
 
 	for i in range(nRow):
 		for j in range(nCol):
 			myGroup = initMap[i,j]
-			locMap = initMap[max(0,i-rVer) : min(nRow,i+rVer+1),:][:,max(0,j-rHor) : min(nCol,j+rHor+1)]
-			wMt = np.copy(weightMat)
+			# print(myGroup)
+			if myGroup == 0:
+				continue
+			else:
+				locMap = initMap[max(0,i-rVer) : min(nRow,i+rVer+1),:][:,max(0,j-rHor) : min(nCol,j+rHor+1)]
+				wMt = np.copy(weightMat)
+				if i < rVer:
+					wMt = wMt[(rVer-i):,:]
+				if (nRow - 1 - i) < rVer:
+					wMt = wMt[:nRow-i+rVer,:]
+				if j < rHor:
+					wMt = wMt[:,(rHor-j):]
+				if (nCol - 1 - j) < rHor:
+					wMt = wMt[:,:nCol-j+rHor]
+				
+				locLikeMap = np.zeros_like(locMap, float)
+				for k in range(np.size(locMap,0)):
+					for l in range(np.size(locMap,1)):
+						locLikeMap[k,l] = likeMat[myGroup][locMap[k,l]]
 
-			if i < rVer:
-				wMt = wMt[(rVer-i):,:]
-				# print(wMt)
-			if (nRow - 1 - i) < rVer:
-				wMt = wMt[:nRow-i+rVer,:]
-				# print(wMt)
-			if j < rHor:
-				wMt = wMt[:,(rHor-j):]
-				# print(wMt)
-			if (nCol - 1 - j) < rHor:
-				wMt = wMt[:,:nCol-j+rHor]
-			happiness = locMap * wMt
-			print(locMap)
-			print(wMt)
-			print(myGroup)
-			print()
+				locHappMap = locLikeMap * wMt
+				happ = locHappMap.sum()
+
+				maxLocHappMap = np.ones_like(locMap, float) * wMt
+				maxHapp = maxLocHappMap.sum()
+
+				happMat[i,j] = happ / maxHapp
+	return happMat
 
 
+nWidth = 10
+occupRate = 0.8
+groupRatios = [.5,.5]
 
+# Weights of impact from neighbors, this person is in the center of te matrix.
+# Rows and columns must be odd numbers.
+
+# Example in class
 weightMat = np.array([[1,1,1],
 					  [1,0,1],
 					  [1,1,1]])
+# weightMat = np.array([[1,1,1,1,1],
+# 					  [1,1,1,1,1],
+# 					  [1,1,0,1,1],
+# 					  [1,1,1,1,1],
+# 					  [1,1,1,1,1]])
 
-weightMat = np.random.rand(5,5)
-print(weightMat)
-calcHappiness(initMap, weightMat)
+# Manhattan distance of 3, impact decreases by distance
+# weightMat = np.array([[1,1,1],
+# 					  [1,0,1],
+# 					  [1,1,1]])
+
+
+# Like Matrix is the likeliness between groups
+# LikeMat[2][3] is how much group 2 likes group 3
+# First row and column is the empty space
+likeMat = [[0.0, 0.0, 0.0],
+		   [0.0, 1.0, 0.3],
+		   [0.0, 0.3, 1.0]]
+
+# likeMat = [[1,0,0],
+# 		   [0,1,0],
+# 		   [0,0,1]]
+
+t = 0.4
+
+initMap = initializeMatrix(nWidth, occupRate, groupRatios)
+happMat = calcHappiness(initMap, weightMat, likeMat)
+
+print(initMap)
+print(happMat)
