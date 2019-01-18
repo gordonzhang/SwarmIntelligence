64dad8c11a53a514650aaad1381b6e4d1d4461df
gordonzhang <gordonzhang81@gmail.com>
2019-01-18 00:19:40 -0500

    completed move function and animation.

--- AggregatingNeighbors.py
+++ AggregatingNeighbors.py
@@ -1,5 +1,8 @@
 import numpy as np
 from random import shuffle
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 
 
 def initializeMatrix(nWidth, occupRate, groupRatios):
@@ -35,7 +38,7 @@ def initializeMatrix(nWidth, occupRate, groupRatios):
 	return np.reshape(nodes, (nWidth, nWidth))
 
 
-def calcHappiness(initMap, weightMat, likeMat):
+def calcHappiness(initMap, weightMat, likeMat, threshold):
 	assert (np.size(weightMat,0)%2 == 1 and np.size(weightMat,1)%2 == 1),...
 	"Number of rows and columns of weight matrix should be both odd numbers."
 
@@ -48,13 +51,15 @@ def calcHappiness(initMap, weightMat, likeMat):
 	rHor = int(round(rHor))
 	
 	happMat = np.zeros_like(initMap, float)
+	vacancyList = []
+	unHappyList = []
 
 	for i in range(nRow):
 		for j in range(nCol):
 			myGroup = initMap[i,j]
 			# print(myGroup)
 			if myGroup == 0:
-				continue
+				vacancyList.append([i,j])
 			else:
 				locMap = initMap[max(0,i-rVer) : min(nRow,i+rVer+1),:][:,max(0,j-rHor) : min(nCol,j+rHor+1)]
 				wMt = np.copy(weightMat)
@@ -73,53 +78,114 @@ def calcHappiness(initMap, weightMat, likeMat):
 						locLikeMap[k,l] = likeMat[myGroup][locMap[k,l]]
 
 				locHappMap = locLikeMap * wMt
-				happ = locHappMap.sum()
+				happVal = locHappMap.sum()
 
-				maxLocHappMap = np.ones_like(locMap, float) * wMt
+				# Maximum happiness is calculated with all 1's in local like map
+				# Happiness is the proportion of max happiness.
+				maxLocHappMap = np.ones_like(locMap, float) * wMt * occupRate
 				maxHapp = maxLocHappMap.sum()
+				happScale = happVal / maxHapp
+				happMat[i,j] = happScale
+				if happScale < threshold:
+					unHappyList.append([i,j])
 
-				happMat[i,j] = happ / maxHapp
-	return happMat
+	return happMat, vacancyList, unHappyList
 
 
-nWidth = 10
-occupRate = 0.8
-groupRatios = [.5,.5]
+def move(initMap, unHappyList, vacancyList, moveSize):
+	moveOutList = []
+	moveInList = []
+	actualMoveSize = min(len(unHappyList), len(vacancyList), moveSize)
+	moveOutIdxList = np.random.choice(len(unHappyList), actualMoveSize, replace=False)
+	for i in moveOutIdxList:
+		moveOutList.append(unHappyList[i])
+
+	moveInIdxList = np.random.choice(len(vacancyList), actualMoveSize, replace=False)
+	for i in moveInIdxList:
+		moveInList.append(vacancyList[i])
+
+	for outIdx, inIdx in zip(moveOutList, moveInList):
+		initMap[outIdx[0], outIdx[1]], initMap[inIdx[0], inIdx[1]] = initMap[inIdx[0], inIdx[1]], initMap[outIdx[0], outIdx[1]]
+	return initMap
+
+
+
 
 # Weights of impact from neighbors, this person is in the center of te matrix.
 # Rows and columns must be odd numbers.
 
 # Example in class
-weightMat = np.array([[1,1,1],
-					  [1,0,1],
-					  [1,1,1]])
-# weightMat = np.array([[1,1,1,1,1],
-# 					  [1,1,1,1,1],
-# 					  [1,1,0,1,1],
-# 					  [1,1,1,1,1],
-# 					  [1,1,1,1,1]])
-
-# Manhattan distance of 3, impact decreases by distance
 # weightMat = np.array([[1,1,1],
 # 					  [1,0,1],
 # 					  [1,1,1]])
+
+# Manhattan distance of 2, impact decreases by distance
+weightMat = np.array([[0.0,0.0,0.5,0.0,0.0],
+					  [0.0,0.5,1.0,0.5,0.0],
+					  [0.5,1.0,0.0,1.0,0.5],
+					  [0.0,0.5,1.0,0.5,0.0],
+					  [0.0,0.0,0.5,0.0,0.0]])
 
 
 # Like Matrix is the likeliness between groups
 # LikeMat[2][3] is how much group 2 likes group 3
 # First row and column is the empty space
 likeMat = [[0.0, 0.0, 0.0],
-		   [0.0, 1.0, 0.3],
-		   [0.0, 0.3, 1.0]]
+		   [0.0, 0.7, 0.3],
+		   [0.0, 0.3, 0.7]]
 
 # likeMat = [[1,0,0],
 # 		   [0,1,0],
 # 		   [0,0,1]]
 
-t = 0.4
+nWidth = 100
+occupRate = 0.9
+groupRatios = [.5,.5]
+threshold = 0.5
+moveSize = 100
+maxSteps = 200
 
 initMap = initializeMatrix(nWidth, occupRate, groupRatios)
-happMat = calcHappiness(initMap, weightMat, likeMat)
-
 print(initMap)
-print(happMat)
+
+# get the percentiles and set the threshold accordingly
+happMat, vacancyList, unHappyList = calcHappiness(initMap, weightMat, likeMat, threshold)
+happList = happMat.reshape(-1)
+index = np.argwhere(happList==0)
+happList = np.delete(happList, index)
+print("Minimun happiness: {}".format(np.amin(happList)))
+
+percList = np.linspace(0, 100, num=11)
+for p in percList:
+	val = np.percentile(happList, p)
+	print("{}% percentile: {}".format(p, val))
+
+threshold = np.percentile(happList, 20)
+
+#############################################################################################
+# Plot
+#############################################################################################
+fig, ax = plt.subplots()
+ims = []
+cmap = matplotlib.colors.ListedColormap(['white', 'cyan', 'blue', 'green'])
+# cmap = matplotlib.colors.Colormap('jet')
+
+for s in range(maxSteps):
+	happMat, vacancyList, unHappyList = calcHappiness(initMap, weightMat, likeMat, threshold)
+	avgHappiness = happMat.mean() / occupRate
+	if len(unHappyList) == 0:
+		print("Everyone's happy! Step: {}".format(s))
+		break
+	initMap = move(initMap, unHappyList, vacancyList, moveSize)
+	print("step: {}".format(s))
+	print(initMap)
+	title = ax.text(0.5,1.05,"Step: {}, Avg Happiness: {}".format(s, round(avgHappiness,3)), 
+                    size=plt.rcParams["axes.titlesize"],
+                    ha="center", transform=ax.transAxes, )
+	ims.append([ax.imshow(initMap, interpolation='nearest', origin='lower',
+                cmap=cmap, animated=True), title])
+
+
+ani = animation.ArtistAnimation(fig, ims, interval=100, blit=False, repeat=True)
+# ani.save('aggregation1.mp4')
+plt.show()
