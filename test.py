9c32561951bdbdb3ab51cc1495f8a1083e28462d
gordon <gordonzhang81@gmail.com>
2019-01-21 20:04:48 -0500

    new function: happiness of each group

--- test.py
+++ test.py
@@ -3,8 +3,19 @@ from numpy import sin, cos, deg2rad
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
-x = (2, 3)
 
-rMat = np.array([[cos(deg2rad(-30)), -sin(deg2rad(-30))],
-              [sin(deg2rad(-30)), cos(deg2rad(-30))]])
-print(np.matmul(rMat, x))
+def HappinessListOfEachGroup(hMat: np.ndarray, aggMap: np.ndarray, gRatios: list):
+	nG = len(gRatios)
+	hList = []
+	for i in range(nG):
+		iG = i + 1
+		index = np.where(aggMap == iG)
+		hList.append(hMat[index].reshape(-1))
+	return hList
+
+
+x = np.array([[1,2,3],[11,1,13],[21,22,23]])
+y = np.array([[99,81,77],[11,12,13],[21,22,23]])
+
+yy = HappinessListOfEachGroup(y, x, [1,1])
+print(yy)
